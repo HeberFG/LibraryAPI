4dea2925ef41322bfdf1f04e77b45b1c48ceb4c9
HeberFG <20193tn116@utez.edu.mx>
2022-12-08 19:56:40 -0600

    Commit inicial

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('book')->group(function () {
+    Route::get("index", [BookController::class, "index"]);
+    Route::post("store", [BookController::class, "store"]);
+    Route::put("update/{id}", [BookController::class, "update"]);
+    Route::get("find/{id}", [BookController::class, "find"]);
+    Route::delete("delete/{id}", [BookController::class, "delete"]);
+});
+
+
+//Authentication is not required for these endpoints
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+//Authentication is required for these endpoints (apply middleware auth:sanctum)
+Route::group(['middleware' => ["auth:sanctum"]], function () {
+    Route::get('userProfile', [AuthController::class, 'userProfile']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::put('changePassword', [AuthController::class, 'changePassword']);
+    Route::post('addBookReview/{id}', [BookController::class, 'addBookReview']);
+    Route::put('updateBookReview/{id}', [BookController::class, 'updateBookReview']);
+});
